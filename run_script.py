09a7478d540056ca77c09156456546ab3bd99ea6
Lloyd Strohl <lloyd.strohl@sslmda.com>
2018-10-01 15:20:47 -0700

    comments

--- run_script.py
+++ run_script.py
@@ -1,11 +1,13 @@
 # Coffee Experiment
+# 
 import tensorflow as tf
 import pandas as pd
 from data_read import data_read
 # Data read
 #   Call data read function to import and parse spreadsheet
 #   Should result in a pandas dataframe with parameter/feature definitions
+
+
 filename = "CoffeeData"
 data = data_read(filename)
 
-# 
